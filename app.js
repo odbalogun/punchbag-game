7f92ad1c6a7377250b9293f8e592e97c31f91c78
Odun Balogun <15163838+odbalogun@users.noreply.github.com>
2020-10-19 23:27:28 +0100

    Added Punch and Restart methods

---
+++ app.js
@@ -0,0 +1,17 @@
+new Vue({
+  el: '#vue-app',
+  data: {
+    health: 100
+  },
+  methods: {
+    punch: function(){
+      this.health -= 10
+    },
+    restart: function(){
+      this.health = 100
+    }
+  },
+  computed: {
+
+  }
+});
